cd769d576a7f88fbe13cfc707deeb754262d6a2b
Gabriella Jaszkaine <gabriella.jaszkaine@aifleet.com>
2023-07-26 22:42:49 +0200

    lesson05: Refactor sort by prices tests->beforeEach,function

--- cypress/e2e/sortByPrice.cy.js
+++ cypress/e2e/sortByPrice.cy.js
@@ -11,43 +11,60 @@ import 'cypress-map'
 chai.use(require('chai-sorted'))
 
 describe('sorting', () => {
-  it('by price lowest to highest', () => {
+  beforeEach(() => {
     cy.log('**log in**')
     cy.visit('/')
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
     cy.location('pathname').should('equal', '/inventory.html')
+  })
 
-    cy.log('**sort by price low to high**')
-    // sort the items from low to high
-    // confirm the item prices are sorted in ascending order
-    cy.get('[data-test="product_sort_container"]').select('lohi')
-    cy.get('.inventory_item_price')
+  // Sorts items by price
+  // @param {'lohi'| 'hilo'} order
+
+  function sortByPrice(order) {
+    expect(order, 'sort order').to.be.oneOf(['lohi', 'hilo'])
+    cy.log(`**sort by price ${order}**`)
+    cy.get('[data-test="product_sort_container"]').select(order)
+  }
+
+  function getPrices() {
+    return cy
+      .get('.inventory_item_price')
       .map('innerText')
       .mapInvoke('slice', 1)
       .map(Number)
-      .print('prices %o')
-      .should('be.ascending')
+      .print('sorted prices %o')
+  }
+
+  it('by price lowest to highest', () => {
+    // cy.log('**sort by price low to high**')
+    // // sort the items from low to high
+    // // confirm the item prices are sorted in ascending order
+    // cy.get('[data-test="product_sort_container"]').select('lohi')
+    // cy.get('.inventory_item_price')
+    //   .map('innerText')
+    //   .mapInvoke('slice', 1)
+    //   .map(Number)
+    //   .print('prices %o')
+    //   .should('be.ascending')
+    sortByPrice('lohi')
+    getPrices().should('be.ascending')
   })
 
   it('by price highest to lowest', () => {
-    cy.log('**log in**')
-    cy.visit('/')
-    cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
-    cy.location('pathname').should('equal', '/inventory.html')
-
     cy.log('**sort by price low to high**')
     // sort the items from high to low price
     // confirm the item prices are sorted from highest to lowest
-    cy.get('[data-test="product_sort_container"]').select('hilo')
-    cy.get('.inventory_item_price')
-      .map('innerText')
-      .mapInvoke('slice', 1)
-      .map(Number)
-      .print('prices %o')
-      .should('be.descending')
+    // cy.get('[data-test="product_sort_container"]').select('hilo')
+    // cy.get('.inventory_item_price')
+    //   .map('innerText')
+    //   .mapInvoke('slice', 1)
+    //   .map(Number)
+    //   .print('prices %o')
+    //   .should('be.descending')
+    sortByPrice('hilo')
+    getPrices().should('be.descending')
   })
 })
