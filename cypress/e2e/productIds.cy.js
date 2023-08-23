743a0821b4e8875a34f980c33f3e6ebda6014ea9
Gabriella Jaszkaine <gabriella.jaszkaine@aifleet.com>
2023-08-23 15:04:57 +0200

    Les22: Confirm item ids are unique

---
+++ cypress/e2e/productIds.cy.js
@@ -0,0 +1,40 @@
+import { LoginPage } from './loginPage'
+
+describe('Products', () => {
+  // create a small type on the fly using jsdoc comment
+  // just to help type check help us
+  /** @type {{username: string, password: string}} */
+  const user = Cypress.env('users').standard
+  // we can even check if the user object is valid
+  if (!user) {
+    throw new Error('Missing the standard user')
+  }
+
+  // before each test, quickly login the user
+  // or restore the previous user session
+  beforeEach(() => {
+    LoginPage.login(user.username, user.password)
+    cy.visit('/inventory.html')
+    cy.location('pathname').should('equal', '/inventory.html')
+  })
+
+  it('have unique ids', () => {
+    // get all inventory items, there should be more than 3
+    // https://on.cypress.io/get
+    // https://on.cypress.io/should
+    cy.get('.inventory_item')
+      .should('have.length.greaterThan', 3)
+
+      // from each element, get the attribute "data-itemid"
+      // and confirm the ids are unique
+      // https://on.cypress.io/invoke
+      // https://glebbahmutov.com/cypress-examples
+      .invoke('toArray')
+      .then((elements) => elements.map((el) => el.getAttribute('data-itemid')))
+      .then(console.log)
+      .should((ids) => {
+        const unique = Cypress._.uniq(ids)
+        expect(unique, 'unique ids:::').to.deep.equal(ids)
+      })
+  })
+})
